fbf57fd144f7e381e4508d18cf4d6c57ec46ddf8
hiddegrei <grei0005@hz.nl>
2021-09-23 10:50:41 +0200

    d

---
+++ assets/js/even.js
@@ -0,0 +1,13 @@
+
+function isEven(a){
+    console.log(a%2)
+    if(a%2==0){
+        console.log(a , 'is even')
+    }else{
+        console.log(a, " is not even")
+    }
+
+}
+isEven(3)
+
+
---
+++ assets/js/stringexp.js
@@ -0,0 +1,4 @@
+let str="Programming is not so cool"
+
+
+console.log(str.replace("not ",""))
